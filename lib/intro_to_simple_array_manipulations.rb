79ed92ef99fb85fcee975c62c3e34a00a4ff05da
Joshua Morales <joshuapmorales@gmail.com>
2020-12-22 22:16:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,7 +23,7 @@ end
 def pop_with_args(array)
   
     @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-    @small_dogs = @dog_breeds.pop ("Chihuahua", "Shiba Inu")
+    @small_dogs = @dog_breeds.pop[2, 3]
     
 end
   
